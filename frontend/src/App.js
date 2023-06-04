0a79f8174c4ce01f6a2124f7cdbab5456b5a0296
Ilya Vazhenin <heyheyftw@gmail.com>
2023-06-05 00:03:13 +0400

    feat: add redirect on auth and localstorage usage for auth data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import NotFound from './pages/404-page';
 import LoginCard from './pages/login-page';
 import Main from './pages/main-page';
 
+import CurrentUserContext from './utils/auth-context';
+
 function App() {
+  const [user, setUser] = useState({ userName: localStorage.getItem('userName') });
+  console.log(user, 'user in app');
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="login" element={<LoginCard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CurrentUserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="login" element={<LoginCard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CurrentUserContext.Provider>
+    
   );
 }
 
