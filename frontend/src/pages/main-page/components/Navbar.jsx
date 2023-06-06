44aed1be92750fe0c943dd73c45cd41b6e887d12
Ilia Vazhenin <heyheyftw@gmail.com>
2023-06-06 17:22:52 +0400

    feat: add main chat page html template

---
+++ frontend/src/pages/main-page/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <a className="navbar-brand" href="/">Hexlet Chat</a>
+        <button type="button" class="btn btn-primary">Выйти</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/main-page/index.jsx
+++ frontend/src/pages/main-page/index.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CurrentUserContext from '../../utils/auth-context';
+import Navbar from './components/Navbar';
+import ChannelsBox from './components/ChannelsBox';
+import MessagesBox from './components/MessagesBox';
 
 const Main = () => {
   const navigate = useNavigate();
@@ -11,9 +14,16 @@ const Main = () => {
   });
   
   return (
-    <div>
-      <h1>Logged in! Success!</h1>
-      <p>Here's the main page when you are authorized</p>
+    <div className="h-100" id="chat">
+      <div className="d-flex flex-column h-100">
+        <Navbar />
+        <div className="container h-100 my-4 overflow-hidden rounded shadow">
+          <div className="row h-100 bg-white flex-md-row">
+            <ChannelsBox />
+            <MessagesBox />
+          </div>
+        </div>
+      </div>  
     </div>
   );
 };
