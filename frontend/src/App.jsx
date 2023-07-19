c7955574abbba831bfad4700edcf03c0c53c9837
Ilia Vazhenin <heyheyftw@gmail.com>
2023-07-19 12:08:36 +0400

    check: see if tests are passed (removing navbar)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import NotFound from './pages/404';
 import LoginCard from './pages/login';
 import RegisterCard from './pages/registration';
 import ChatMain from './pages/chat';
-import Navbar from './shared-components/Navbar';
+// import Navbar from './shared-components/Navbar';
 import PrivateRoute from './utils/private-route-main';
 import store from './slices/index';
 import socketInstance from './utils/socket-init';
@@ -37,7 +37,7 @@ const App = () => {
         <Provider store={store}>
 
           <BrowserRouter>
-            <Navbar />
+            {/* <Navbar /> */}
             <Routes>
               <Route
                 index
